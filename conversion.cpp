0008b7544a3b1cb1ad729214b1a647c9f9efcc8f
apGit-Hub <90646416+apGit-Hub@users.noreply.github.com>
2021-09-13 14:39:35 -0400

    Update and rename HW03 conversion.cpp to conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -7,6 +7,7 @@ int main()
     double fahrenheit[6];
     cout << "Enter five temperature values in Fahrenheit: \n";
     cin >> fahrenheit[0] >> fahrenheit[1] >> fahrenheit[2] >> fahrenheit[3] >> fahrenheit[4];
+    
     //Setup for the columns and fahrenheit to celsius conversions
     cout << setw(10) << right << "Fahrenheit" << setw(10) << right << "Celsius" << setw(15) << right << "Warning";
     for (int i = 0; i < 5; i++) {
